0bc330ad70a6c6e670911c13021538d516edc6bc
Tad <tadesseakele27@gmail.com>
2023-12-15 17:24:38 +0300

    Implement socket.io

--- client/app/page.js
+++ client/app/page.js
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+"use client";
+import { ModeToggle } from "@/components/mode-toggle";
+import { NotificationList } from "@/components/notification-list";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+const notificationItems = [
+  {
+    id: 1,
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    id: 2,
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    id: 3,
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+];
 
 export default function Home() {
+  const [socket, setSocket] = useState(null);
+  const [results, setResults] = useState([]);
+  const [notifications, setNotifications] = useState(notificationItems);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    setSocket(io("http://192.168.1.201:4000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("message-from-server", (data) => {
+      console.log(data);
+      setResults((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = {
+      id: Math.floor(Math.random() * 100),
+      title: "Teklu",
+      description: "Hello Tad",
+    };
+
+    socket.emit("send-message", { ...body });
+    setNotifications((prev) => [...prev, body]);
+    toast({
+      title: body.title,
+      description: body.description,
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="w-full flex justify-end gap-2">
+          {notifications.length > 0 ? (
+            <Badge variant="secondary" className="border border-slate-700">
+              {notifications.length >= 10
+                ? notifications.length
+                : "0" + notifications.length}
+            </Badge>
+          ) : null}
+          <ModeToggle />
+          <NotificationList
+            notifications={notifications}
+            setNotifications={setNotifications}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h2 className="text-3xl text-slate-300">Socket Client</h2>
+        <Button
+          onClick={handleSubmit}
+          // className="h-10 px-6 font-semibold rounded-md bg-emerald-600 hover:bg-emerald-500 transition-all text-white"
+          type="submit"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          Submit
+        </Button>
+        <article className="flex flex-col">
+          {notifications.map((notification, index) => (
+            <div
+              key={index}
+              className="border-2 border-slate-900 bg-slate-950 px-2 py-1 min-w-[200px] m-2 rounded-md"
+            >
+              <h3 className="text-slate-300 text-xl">{notification.title}</h3>
+              <p className="text-slate-400">{notification.description}</p>
+            </div>
+          ))}
+        </article>
+      </main>
+      <Toaster />
+    </>
+  );
 }
