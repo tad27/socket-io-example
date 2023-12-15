8fede3f4c09beb9d65ab8888beb3a408c49bef12
Tad <tadesseakele27@gmail.com>
2023-12-15 17:23:11 +0300

    Add  socket.io to the server.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3008
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
-app.get('/', (req, res) => {
-  res.send('Hello Tad!')
-})
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const port = 4000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Tade!");
+});
+
+io.on("connection", (socket) => {
+  socket.on("send-message", (data) => {
+    // Save to the database here
+    socket.emit("message-from-server", data);
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log("User left!");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
